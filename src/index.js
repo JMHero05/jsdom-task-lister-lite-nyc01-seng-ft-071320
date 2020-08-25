47cba2f2e14a4166393fcaa04b697dfefdb95c18
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-08-25 14:53:06 -0400

    core done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const submit = document.querySelector('#create-task-form');
-  const sbutton = submit.querySelectorAll('input')[1];
+  const des = document.querySelectorAll('input')[0];
+  const sButton = submit.querySelectorAll('input')[1];
   const list = document.querySelector('#tasks');
 
+  
+  sButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    list.target
+    let toDoLi = document.createElement('li');
+    let listItem = list.appendChild(toDoLi);
+    listItem.innerText = des.value;
+    
+  })
+  
 });
+
+
+
+
+
+
+// let addToDo = function(e) {
+//   const sButt = e.target;
+//   sButt.append()
+// }
+// function addTextNode(inputVal) {
+//   const list = document.querySelector('#tasks');
+//   let toDoLi = document.createElement('li');
+//   let listItem = list.appendChild(toDoLi);
+//   listItem.innerText = inputVal;
+// }
+
+// function addTextNode(text) {
+//   // let newText = document.createTextNode(text);
+//   const list = document.querySelector('#tasks');
+
+
+//   let toDoLi = document.createElement('li');
+//   let listItem = list.appendChild(toDoLi);
+//   listItem.innerText = text;
+// }
+
+
+

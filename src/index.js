41256b00328debd67fe573a81d52007f2d7f2548
Jake Mills <jake.mills.05@gmail.com>
2020-08-25 16:49:40 -0400

    Added instructor solution above our commented out solution (We got first deliverable, and we were close to first Stretch goal)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const submit = document.querySelector('#create-task-form');
-  const des = document.querySelectorAll('input')[0];
-  const sButton = submit.querySelectorAll('input')[1];
-  const list = document.querySelector('#tasks');
-
-  
-  sButton.addEventListener('click', function(e) {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
-    list.target
-    let toDoLi = document.createElement('li');
-    let listItem = list.appendChild(toDoLi);
-    listItem.innerText = des.value;
-    
+
+    const task = e.target['new-task-description'].value
+    const taskLi = document.createElement('li')
+    taskLi.textContent = task
+    const button = document.createElement('button')
+    button.textContent = 'X'
+    taskLi.append(button)
+    button.addEventListener('click', function (e) {
+      const taskLi = e.target.parentElement
+      taskLi.remove()
+    })
+    const tasks = document.getElementById('tasks')
+    tasks.append(taskLi)
   })
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const submit = document.querySelector('#create-task-form');
+  // const des = document.querySelectorAll('input')[0];
+  // const sButton = submit.querySelectorAll('input')[1];
+  // const list = document.querySelector('#tasks');
+  // const libutt = document.querySelector("#tasks > li > button");
+  // const listItem = document.querySelector('#tasks > li')
+  // const button = document.getElementsByTagName(button)
+
+  // sButton.addEventListener('click', function (e) {
+  //   e.preventDefault();
+  //   list.target
+  //   let toDoLi = document.createElement('li');
+  //   let listItem = list.appendChild(toDoLi);
+  //   listItem.innerHTML = `
+  //   ${des.value}
+  //   <button data-description="${des.value}">X</button>
+  //   `
+  // })
+
+  // This is where we tried to implement instructor solution,
+  // but did not completely work with the solution we had for
+  // initial deliverables.
+  // button.addEventListener('click', function (e) {
+  //   const taskLi = e.target.parentElement
+  //   taskLi.remove()
+  // })
+
+  // libutt.forEach(addEventListener('click', function (e) {
+  //     const listItem = e.target;
+  //     if (libutt[])
+  //   })
+
+
 });
 
 
@@ -42,6 +90,3 @@ document.addEventListener("DOMContentLoaded", () => {
 //   let listItem = list.appendChild(toDoLi);
 //   listItem.innerText = text;
 // }
-
-
-
